8c1f37b0027bf1132aaf4ef2f05e42542d6d3c20
Chandrashekar112 <chandrashekar.sukkali@ness.com>
2021-05-21 12:27:24 +0530

    'nodejsapplication'

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv/config");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true })).use(bodyParser.json());
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Successfull run allication");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.end();
+  // res.send("Hello, welcome to Node js Developement");
+});
+
+app.listen(port, () => {
+  console.log("Server running Successfully in the port", port);
+});

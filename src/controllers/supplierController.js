464cb4639410e5af975a4bd8be265b106fa08a7b
Chandrashekar112 <chandrashekar.sukkali@ness.com>
2021-06-28 21:06:05 +0530

    'supplierChnages'

--- src/controllers/supplierController.js
+++ src/controllers/supplierController.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const pool = require('./config');
 
 
@@ -25,21 +26,19 @@ const updateSupplier = async(req, res) => {
     };
     const brand = req.params.id;
     const { supplier } = req.body;
+    let date = new Date();
+    let newDate= new Date(date.toLocaleString('en-US', { timeZone: 'America/Chicago' }));
+    let last_updated = moment(newDate).format('YYYY-MM-DD hh:mm:ss');
 
-    console.log(req.body);
-    var currentUtcTime = new Date();
-    var currentDateTimeCentralTimeZone = new Date("",currentUtcTime.toLocaleString('en-US', { timeZone: 'America/Chicago' }));
-    console.log(currentDateTimeCentralTimeZone);
-
-    // await pool.query('UPDATE rb.supplier_map2 SET supplier=$1 WHERE vendor=$2', [supplier, brand], (error, results) => {
-    //     if (error) throw error;
-    //     returnMessage.isError = false;
-    //     returnMessage.message = `Successfully Updated the supplier ${brand}`;
-    //     returnMessage.data = results.rows;
-    //     res.status(200).json(returnMessage);
-    //     res.end();
-    // })
-    
+    await pool.query('UPDATE rb.supplier_map2 SET supplier=$1,last_updated=$2,updated_by=$3 WHERE vendor=$4', [supplier,last_updated,"RBUI",brand], (error, results) => {
+        if (error) throw error;
+        returnMessage.isError = false;
+        returnMessage.message = `Successfully Updated the supplier ${brand}`;
+        returnMessage.data = results.rows;
+        res.status(200).json(returnMessage);
+        res.end();
+    })
+ 
 }
 
 
@@ -49,7 +48,7 @@ const getUnmappedBrands = async(req, res) => {
         data: null,
         message: "Error occured",
     };
-    await pool.query('select supplier, vendor as brand, last_updated::varchar, updated_by from rb.supplier_map2 UNION SELECT distinct NULL, vendor AS brand, NULL, NULL FROM rb.shopify_order_item WHERE vendor not IN (SELECT vendor FROM rb.supplier_map2) ORDER BY supplier', (error, results) => {
+    await pool.query('SELECT distinct NULL, vendor AS brand, NULL, NULL FROM rb.shopify_order_item WHERE vendor not IN (SELECT vendor FROM rb.supplier_map2)', (error, results) => {
         if (error) throw error;
         returnMessage.isError = false;
         returnMessage.message = "Records found";
@@ -60,12 +59,66 @@ const getUnmappedBrands = async(req, res) => {
     
 }
 
+
+const updateUnmappedBrands = async (req, res) => {
+    const returnMessage = {
+        isError: true,
+        data: null,
+        message: "Error occured",
+    };
+
+    const { brand, supplier } = req.body;
+
+    let date = new Date();
+    let newDate= new Date(date.toLocaleString('en-US', { timeZone: 'America/Chicago' }));
+    let last_updated = moment(newDate).format('YYYY-MM-DD hh:mm:ss');
+
+    
+    await pool.query("INSERT INTO rb.supplier_map2 (supplier,vendor,create_time,last_updated,updated_by) VALUES ($1,$2,$3,$4,$5)",[supplier,brand,last_updated,last_updated,"RBUI"], (error,results) => {
+        if (error) throw error;
+        returnMessage.isError = false;
+        returnMessage.message = `Successfully update the Brand`;
+        returnMessage.data = results.rows;
+        res.status(200).json(returnMessage);
+        res.end();
+    })
+    
+}
+
+
+
+const addSupplier = async(req,res) => {
+    const returnMessage = {
+        isError: true,
+        data: null,
+        message: "Error occured",
+    };
+    const { supplier, brand} = req.body;
+    let date = new Date();
+    let newDate= new Date(date.toLocaleString('en-US', { timeZone: 'America/Chicago' }));
+    let last_updated = moment(newDate).format('YYYY-MM-DD hh:mm:ss');
+    await pool.query('INSERT INTO rb.supplier_map2 (supplier,vendor,create_time,last_updated,updated_by) VALUES ($1,$2,$3,$4,$5)',[supplier,brand,last_updated,last_updated,"RBUI"], (error,results) => {
+        if (error) throw error;
+        returnMessage.isError = false;
+        returnMessage.message = "Successfully save the Brand";
+        returnMessage.data = results.rows;
+        res.status(200).json(returnMessage);
+        res.end();
+    });
+
+}
+
+
+
+
   
 
 module.exports = {
     getSupplier,
     updateSupplier,
+    addSupplier,
     getUnmappedBrands,
+    updateUnmappedBrands
   
 
 }
--- src/routers/supplier.js
+++ src/routers/supplier.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
-const { getSupplier,updateSupplier,getUnmappedBrands } = require('../controllers/supplierController');
+const { getSupplier,updateSupplier,addSupplier,getUnmappedBrands,updateUnmappedBrands } = require('../controllers/supplierController');
 
 router.get('/', getSupplier);
 router.put('/:id', updateSupplier);
+router.post('/', addSupplier);
 router.get('/unmappedBrands', getUnmappedBrands);
+router.post('/unmappedBrands', updateUnmappedBrands);
 
 
 module.exports = router;

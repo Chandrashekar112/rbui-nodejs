c9754b8239ccaeda6f126a9c00012ee4746b24e4
Chandrashekar112 <chandrashekar.sukkali@ness.com>
2021-07-08 15:13:16 +0530

    'retailerchanges'

--- src/routers/retailer.js
+++ src/routers/retailer.js
@@ -10,8 +10,6 @@ const {
 } = require("../controllers/retailerController");
 
 router.get("/", getRetailer);
-// router.get("/company_ids", getCompanyId);
-// router.get("/retailer_names", getRetailerName);
 router.get("/retailer_state", RetailerState);
 router.post("/addRetailer", CreateRetailer);
 router.put("/:id", UpdateRetailer);

cb49cb05fabf36a8bd845d1f6303e1019191f8f5
Chandrashekar112 <chandrashekar.sukkali@ness.com>
2021-07-23 13:58:06 +0530

    'signinchanges'

--- src/controllers/signinController.js
+++ src/controllers/signinController.js
@@ -1,6 +1,6 @@
 const pool = require('./config');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
 const USERS = require('./User');
 
